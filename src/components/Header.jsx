939959e1bba76a4cb42c7822fe89d1d3d4fdc935
Dexter Dick <@dexterdick@hotmail.com>
2023-03-04 15:36:31 -0500

    added bg img header / css/nav to project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Header.css";
+import Navigation from "./Navigation";
+
+function Header(props) {
+    return (
+        <header className="header">
+            <h1>Dexter's portfolio </h1>
+            <Navigation />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#/">About Me</a>
+                </li>
+                <li>
+                    <a href="#/">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#/">Contact</a>
+                </li>
+                <li>
+                    <a href="#/">Resume</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
